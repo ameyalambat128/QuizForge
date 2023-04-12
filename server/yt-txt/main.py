328d0503011bab55bb2f13c1390f5335aad43f91
Ameya Lambat <63185829+ameyalambat128@users.noreply.github.com>
2023-04-12 15:37:47 -0700

    feat: pdf to text func

--- server/yt-txt/main.py
+++ server/yt-txt/main.py
@@ -4,10 +4,13 @@ from dotenv import load_dotenv
 from pytube import YouTube
 from flask import Flask, render_template, request, redirect, url_for
 from whisper import convert_link_to_audio
+import PyPDF2
 
 load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+app = Flask(__name__)
 
 def convert_link_to_audio(url):
     yt = YouTube(url)
@@ -27,11 +30,28 @@ def convert_link_to_audio(url):
     print(transcript)
     
     #os.remove(newfile)
-    
 
-app = Flask(__name__)
+# Open the PDF file in read-binary mode
+def pdf_to_text():
+    with open('example.pdf', 'rb') as pdf_file:
+        
+        # Create a PDF object to read the file
+        pdf_reader = PyPDF2.PdfFileReader(pdf_file)
+        
+        # Initialize an empty string to store the text
+        text = ""
+        
+        # Loop through each page in the PDF file
+        for page in range(pdf_reader.getNumPages()):
+            
+            # Extract the text from the page and append it to the text string
+            text += pdf_reader.getPage(page).extractText()
+            
+    # Print the extracted text
+    print(text)
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route('/yt', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         input_text = request.form['input_text']
@@ -44,6 +64,10 @@ def index():
 
 def store_input(input_text):
     print(f"Stored input: {input_text}")
+
+@app.route('/')
+def main():
+    return {'message': 'Welcome to Quiz Forge API'}
 
 if __name__ == '__main__':
     app.run(debug=True)
